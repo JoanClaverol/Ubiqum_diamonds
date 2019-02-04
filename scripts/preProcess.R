f240c9141e9a1ae400fcd145cdb8d5f5c1b76deb
DESKTOP-564QIFI\romcl <joanclaverol@gmail.com>
2019-02-04 20:17:49 +0100

    final commit

--- scripts/preProcess.R
+++ scripts/preProcess.R
@@ -30,6 +30,26 @@ diamonds_dummy %<>%
 # loading the model 
 load("scripts/model.rds")
 
-# applying the model 
+# applying the model and storing the results 
 results <- as.data.frame(predict(object = mod_final, newdata = diamonds_dummy))
-results$
+colnames(results) <- "log_pred"
+
+# adding actual prices
+results$price <- diamonds_dummy$price
+results$pred_price <- exp(results$log_pred)
+
+
+# checking results ----
+
+# regression metrics 
+postResample(results$price, results$pred_price)
+
+# ploting errors
+plot(results$price ~ results$pred_price)
+abline(a = 1, b = 1, col = "red")
+
+# detecting the opportunities to buy
+results %<>% 
+  mutate(errors = price - pred_price) %>% 
+  arrange(errors) %>% as_tibble()
+
